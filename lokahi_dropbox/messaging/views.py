5d54bb2cc917bf6df951e52c52eb56d14c3c9e76
DanielSBrown <dsb9ef@virginia.edu>
2017-04-12 18:47:24 -0400

    Change a lot about messages...POST from GET, adding subject line, CSRF stuff, etc.

--- lokahi_dropbox/messaging/views.py
+++ lokahi_dropbox/messaging/views.py
@@ -7,17 +7,19 @@ from django.contrib.auth.models import User
 # Create your views here.
 @csrf_protect
 def send_message(request):
-    if request.method == 'GET':
-        form = MessageForm(request.GET)
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
         if form.is_valid():
             m = form.cleaned_data['message']
             r = form.cleaned_data['receiver']
             form.validate()
             s = request.user.username
-            message = Message.objects.create(message=m, receiver=r, sender=s)
-            return render_to_response('messaging/send_message.html',)
-        else:
-            return render_to_response('messaging/send_message.html',)
+            p = form.cleaned_data['subject']
+            message = Message.objects.create(subject=p,message=m, receiver=r, sender=s)
+            return render(request, 'messaging/send_message.html', {'form': form})
+    else:
+        form = MessageForm()
+        return render(request,'messaging/send_message.html', {'form': form})
 
 @csrf_protect
 def receive_message(request):
