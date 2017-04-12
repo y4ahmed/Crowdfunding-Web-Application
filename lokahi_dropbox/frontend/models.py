6c374e42d44f991ab259b2d2a21cc89c12add90a
DanielSBrown <dsb9ef@virginia.edu>
2017-04-12 16:56:29 -0400

    Fixed database stuff. Should work now

--- lokahi_dropbox/frontend/models.py
+++ lokahi_dropbox/frontend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class BaseUser(models.Model):
+    user = models.OneToOneField(User)
+    user_role = models.CharField(max_length=30)
+
+    def set_user_role(self, user_role):
+        self.user_role = user_role
