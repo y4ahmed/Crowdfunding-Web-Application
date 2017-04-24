7000cc7f8cc63dde8be41e3ef6fcb1113f7c60c9
KeanF <kf9zy@virginia.edu>
2017-04-24 14:26:11 -0400

    Add basic Site Manager user file

--- lokahi_dropbox/frontend/views.py
+++ lokahi_dropbox/frontend/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
-from frontend.forms import *
+from frontend.forms import RegistrationForm
 from .models import BaseUser
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.views.decorators.csrf import csrf_protect
@@ -10,6 +11,7 @@ from django.template import RequestContext
 from .forms import UploadFileForm
 from Crypto.PublicKey import RSA
 
+
 @csrf_protect
 def register(request):
     if request.method == 'POST':
@@ -18,28 +20,32 @@ def register(request):
             user_role = form.cleaned_data['user_role']
 
             user = User.objects.create_user(
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email']
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
             )
-            base = BaseUser.objects.create(user=user, user_role=user_role, RSAkey=RSA.generate(2048).exportKey())
+            base = BaseUser.objects.create(
+                user=user, user_role=user_role,
+                RSAkey=RSA.generate(2048).exportKey()
+            )
 
             return HttpResponseRedirect('/register/success/')
     else:
         form = RegistrationForm()
+
     variables = RequestContext(request, {
-    'form': form
+        'form': form
     })
 
     return render_to_response(
-    'registration/register.html',
-    variables,
+        'registration/register.html',
+        variables,
     )
 
 
 def register_success(request):
     return render_to_response(
-    'registration/success.html',
+        'registration/success.html',
     )
 
 
@@ -58,8 +64,8 @@ def logout_page(request):
 def home(request):
     base = BaseUser.objects.get(user=request.user)
     return render_to_response(
-    'home.html',
-    { 'user': base, 'type':base.user_role }
+        'home.html',
+        {'user': base, 'type': base.user_role}
     )
 
 
@@ -76,7 +82,8 @@ def upload_file(request):
 
 
 def handle_uploaded_file(f):
-    file_extension = f._get_name()[-3:]                         # Gets the file extension using substrings
+    # Gets the file extension using substrings
+    file_extension = f._get_name()[-3:]
     with open('lokahi_dropbox/temp.' + file_extension, 'wb+') as destination:
         for chunks in f.chunks():
             destination.write(chunks)
