57f05481b41209143c4ea8cf268a9c4ed3e37f85
KeanF <kf9zy@virginia.edu>
2017-04-29 23:48:15 -0400

    Refactor urls from lokahi_dropbox/urls.py

---
+++ lokahi_dropbox/frontend/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^home/$', views.home, name='home'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register/success/$', views.register_success,
+        name='register_success'),
+    url(r'^logout/$', views.logout_page, name='logout'),
+
+    # Temporarily handle broken links
+    url(r'^upload/$', views.upload_file, name='upload_file'),
+    url(r'^upload_success/$', views.upload_success, name='upload_succes'),
+    url(r'^create_report/$', views.home, name='create_report'),
+    url(r'^view_reports/$', views.home, name='view_reports'),
+]
