bc9cadbdd0908daece786c5eefb8163cf8ea3380
KeanF <kf9zy@virginia.edu>
2017-04-30 00:12:02 -0400

    PEP-8 standarization

--- lokahi_dropbox/frontend/forms.py
+++ lokahi_dropbox/frontend/forms.py
@@ -10,19 +10,32 @@ class RegistrationForm(forms.Form):
         ("Company User", _("Company User")),
         ("Investor User", _("Investor User")),
     )
-
-    username = forms.RegexField(regex=r'^\w+$',
-                                widget=forms.TextInput(
-                                    attrs={'required': True, 'max_length': 30, 'class': 'form-control'}),
-                                label=_("Username"), error_messages={
-                                    'invalid': _("This value must contain only letters, numbers and underscores.")})
-    email = forms.EmailField(widget=forms.TextInput(attrs={'required': True, 'max_length': 30, 'class': 'form-control'}),
-                             label=_("Email address"))
-    password1 = forms.CharField(widget=forms.PasswordInput(
-        attrs={'required': True, 'max_length': 30, 'render_value': False, 'class': 'form-control'}), label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput(
-        attrs={'required': True, 'max_length': 30, 'render_value': False, 'class': 'form-control'}),
-        label=_("Password (again)"))
+    # Keep common field attributes in a single place
+    common_attrs = {'required': True,
+                    'max_length': 30, 'class': 'form-control'}
+    pass_attrs = {'required': True, 'max_length': 30,
+                  'render_value': False, 'class': 'form-control'}
+    # Form fields
+    username = forms.RegexField(
+        regex=r'^\w+$',
+        widget=forms.TextInput(attrs=common_attrs),
+        label=_("Username"),
+        error_messages={
+            'invalid': _("This value must contain only letters, numbers and underscores.")
+        }
+    )
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs=common_attrs),
+        label=_("Email address")
+    )
+    pass1 = forms.CharField(
+        widget=forms.PasswordInput(attrs=pass_attrs),
+        label=_("Password")
+    )
+    pass2 = forms.CharField(
+        widget=forms.PasswordInput(attrs=pass_attrs),
+        label=_("Password (again)")
+    )
     user_role = forms.ChoiceField(choices=role_choices)
 
     def clean_username(self):
@@ -35,8 +48,8 @@ class RegistrationForm(forms.Form):
             _("The username already exists. Please try another one."))
 
     def clean(self):
-        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
-            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+        if 'pass1' in self.cleaned_data and 'pass2' in self.cleaned_data:
+            if self.cleaned_data['pass1'] != self.cleaned_data['pass2']:
                 raise forms.ValidationError(
                     _("The two password fields did not match."))
         return self.cleaned_data
@@ -44,5 +57,7 @@ class RegistrationForm(forms.Form):
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(widget=forms.TextInput(
-        attrs={'required': True, 'max_length': 30}), label=_("File Name"))
+        attrs={'required': True, 'max_length': 30}),
+        label=_("File Name")
+    )
     file = forms.FileField(label=_("File"), allow_empty_file=True)
