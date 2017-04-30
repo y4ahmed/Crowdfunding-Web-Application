a51f0c5e764b87097c41b059d557333952888148
y4ahmed <ysa3bb@virginia.edu>
2017-04-30 19:58:54 -0400

    Fixed Import Error, Circular Dependence

--- lokahi_dropbox/frontend/models.py
+++ lokahi_dropbox/frontend/models.py
@@ -10,7 +10,7 @@ class BaseUser(models.Model):
     user = models.OneToOneField(User)
     user_role = models.CharField(max_length=30)
     RSAkey = models.CharField(max_length=10000)
-   # reports = models.ManyToManyField(Report)
+    reports = models.ManyToManyField('reports.Report')
 
     def set_user_role(self, user_role):
         self.user_role = user_role
