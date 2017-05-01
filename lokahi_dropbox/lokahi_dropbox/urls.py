f374ae18f19bb3979750dde95ed0b14d7e6a4c65
KeanF <kf9zy@virginia.edu>
2017-04-30 22:10:58 -0400

    Resolve urls

--- lokahi_dropbox/lokahi_dropbox/urls.py
+++ lokahi_dropbox/lokahi_dropbox/urls.py
@@ -11,14 +11,15 @@ urlpatterns = [
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     # url(r'^admin/', include(admin.site.urls)),
 
+    # App urls
     url(r'^', include('frontend.urls'), name='home'),
     url(r'^messaging/', include('messaging.urls'), name='messaging'),
     url(r'^groups/', include('groups.urls'), name='groups'),
     url(r'^wall/', include('wall_post.urls'), name='wall'),
     url(r'^manage_site/', include('site_manager.urls'), name='manage_site'),
-    url(r'^reports/', include ('reports.urls'), name = 'reports'),
-    #url(r'^createReport/', createReport),
-    #url(r'^viewReport/', viewReport),
+    url(r'^reports/', include('reports.urls'), name='reports'),
+    # url(r'^createReport/', createReport),
+    # url(r'^viewReport/', viewReport),
     # Search views
     url(r'^home/search/$', basic_search),
 ]
