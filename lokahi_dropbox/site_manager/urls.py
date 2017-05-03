534ef780094142a4e763bdeff8662ac37c87aa96
KeanF <kf9zy@virginia.edu>
2017-05-02 22:11:34 -0400

    Remove extraneous stuff

--- lokahi_dropbox/site_manager/urls.py
+++ lokahi_dropbox/site_manager/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
         name='give_site_manager_role'),
     url(r'^suspend_user/$', views.suspend_user, name='suspend_user'),
     url(r'^give_site_manager_role/$', views.restore_user, name='restore_user'),
-    # url(r'^create_group/$', views.create_group, name='create_group'),
-    # url(r'^view_groups/$', views.view_group, name='view_group'),
-    # url(r'^edit_group/$', views.edit_group, name='edit_group'),
-    # url(r'^add_members/$', views.add_members, name='add_members'),
 ]
