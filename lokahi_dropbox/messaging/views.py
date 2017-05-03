f51d5fe1121877cdc41356a29ac24f40e112d339
KeanF <kf9zy@virginia.edu>
2017-05-02 22:17:45 -0400

    Resolve minor data input issue

--- lokahi_dropbox/messaging/views.py
+++ lokahi_dropbox/messaging/views.py
@@ -9,11 +9,17 @@ from Crypto.PublicKey import RSA
 
 @csrf_protect
 def messaging(request):
-    return render(request, 'messaging/messaging.html')
+    base = BaseUser.objects.get(user=request.user)
+    return render(
+        request,
+        'messaging/messaging.html',
+        {'type': base.user_role}
+    )
 
 
 @csrf_protect
 def send_message(request):
+    base = BaseUser.objects.get(user=request.user)
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
@@ -24,7 +30,12 @@ def send_message(request):
             p = form.cleaned_data['subject']
 
             if r == s:
-                return render(request, 'messaging/send_message.html', {'form': MessageForm(), 'is_yourself': True})
+                return render(
+                    request,
+                    'messaging/send_message.html',
+                    {'form': MessageForm(), 'type': base.user_role,
+                     'is_yourself': True}
+                )
                 # raise Error("cannot send a message to yourself")
 
             if form.cleaned_data['encrypt']:
@@ -36,27 +47,47 @@ def send_message(request):
                     m.encode('utf-8'), "not needed".encode('utf-8'))[0])
                 message = Message.objects.create(
                     subject=p, message=m, receiver=r, sender=s, encrypt=True)
-                return render(request, 'messaging/send_message.html', {'form': MessageForm(), 'is_yourself': False})
+                return render(
+                    request,
+                    'messaging/send_message.html',
+                    {'form': MessageForm(), 'type': base.user_role,
+                     'is_yourself': False}
+                )
             else:
                 # leave as the same
                 message = Message.objects.create(
                     subject=p, message=m, receiver=r, sender=s, encrypt=False)
-                return render(request, 'messaging/send_message.html', {'form': MessageForm(), 'is_yourself': False})
+                return render(
+                    request,
+                    'messaging/send_message.html',
+                    {'form': MessageForm(), 'type': base.user_role,
+                     'is_yourself': False}
+                )
     else:
         form = MessageForm()
-        return render(request, 'messaging/send_message.html', {'form': form, 'is_yourself': False})
+        return render(
+            request,
+            'messaging/send_message.html',
+            {'form': form, 'type': base.user_role, 'is_yourself': False}
+        )
 
 
 @csrf_protect
 def receive_message(request):
+    base = BaseUser.objects.get(user=request.user)
     messages = Message.objects.filter(receiver=request.user.username)
     if len(messages) != 0:
-        return render(request, 'messaging/receive_message.html', {'messages': messages})
+        return render(
+            request,
+            'messaging/receive_message.html',
+            {'messages': messages, 'type': base.user_role}
+        )
     else:
         return render(request, 'messaging/receive_message.html',)
 
 
 def delete_message(request):
+    base = BaseUser.objects.get(user=request.user)
     if request.method == 'POST':
         form = DeleteForm(data=request.POST)
         if form.is_valid():
@@ -65,10 +96,15 @@ def delete_message(request):
             m = Message.objects.get(id=index)
             m.delete()
             messages = Message.objects.filter(receiver=request.user.username)
-            return render(request, 'messaging/receive_message.html', {'messages': messages})
+            return render(
+                request,
+                'messaging/receive_message.html',
+                {'messages': messages, 'type': base.user_role}
+            )
 
 
 def decrypt_message(request):
+    base = BaseUser.objects.get(user=request.user)
     if request.method == 'POST':
         form = DecryptMessageForm(data=request.POST)
         if form.is_valid():
@@ -76,8 +112,9 @@ def decrypt_message(request):
             if message.encrypt:
                 # attempt decrypting only if it was encrypted to begin with
                 # user your private key for decryption
-                # set the message encrypt field to false now so that we dont try to decrypt it again
-                # replace the message body and the encrypt fields
+                # set the message encrypt field to false now so that we don't
+                # try to decrypt it again replace the message body and the
+                # encrypt fields
                 user = User.objects.get(username=request.user.username)
                 base_user = BaseUser.objects.get(user=user)
                 # print(base_user.RSAkey)
@@ -91,9 +128,18 @@ def decrypt_message(request):
                 # throw error
                 messages = Message.objects.filter(
                     receiver=request.user.username).order_by('id')
-                return render(request, 'messaging/receive_message.html', {'messages': messages, 'is_decrypted': True})
+                return render(
+                    request,
+                    'messaging/receive_message.html',
+                    {'messages': messages, 'type': base.user_role,
+                     'is_decrypted': True}
+                )
 
             messages = Message.objects.filter(
                 receiver=request.user.username).order_by('id')
 
-    return render(request, 'messaging/receive_message.html', {'messages': messages, 'is_decrypted': False})
+    return render(
+        request,
+        'messaging/receive_message.html',
+        {'messages': messages, 'type': base.user_role, 'is_decrypted': False}
+    )
